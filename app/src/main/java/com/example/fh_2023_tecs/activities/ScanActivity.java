7c1464adc7d789ac5c92014bba4429e18406471b
Emily Crowl <emilycrowl2k@gmail.com>
2023-04-08 23:45:29 -0700

    all ready for the db, meanwhile i do ui

--- app/src/main/java/com/example/fh_2023_tecs/activities/ScanActivity.java
+++ app/src/main/java/com/example/fh_2023_tecs/activities/ScanActivity.java
@@ -1,25 +1,45 @@
 package com.example.fh_2023_tecs.activities;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 
 import com.example.fh_2023_tecs.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.parse.ParseUser;
 
+import java.io.File;
+
 import fragments.HomeFragment;
 import fragments.ProfileFragment;
 import fragments.WikiFragment;
 
 public class ScanActivity extends AppCompatActivity {
+    private static final String TAG = "ScanActivity";
+    private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 396;
     private BottomNavigationView bottomNavigationView;
+    Button btnCapture;
+    ImageView ivItem;
+    private File photoFile;
+    public String photoFileName = "item.jpg";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,6 +49,8 @@ public class ScanActivity extends AppCompatActivity {
         actionBar = getSupportActionBar();
 
         bottomNavigationView = findViewById(R.id.bottomNavigation);
+        btnCapture = findViewById(R.id.btnCapture);
+        ivItem = findViewById(R.id.ivItem);
 
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
@@ -55,7 +77,65 @@ public class ScanActivity extends AppCompatActivity {
                 }
             }
         });
+
+        btnCapture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchCamera();
+            }
+        });
     }
+
+    private void launchCamera() {
+        //create Intent to take a picture and return control to the calling application
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        //create a File reference for future access
+        photoFile = getPhotoFileUri(photoFileName);
+
+        //wrap File object into a content provider
+        //required for API >= 24
+        //see https://guides.codepath.com/android/Sharing-Content-with-Intents#sharing-files-with-api-24-or-higher
+        Uri fileProvider = FileProvider.getUriForFile(this, "com.codepath.fileprovider", photoFile);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
+
+        //if you call startActivityForResult() using an intent that no app can handle, your app will crash.
+        //so as long as the result is not null, it's safe to use the intent.
+        if (intent.resolveActivity(this.getPackageManager()) != null) {
+            //start the image capture intent to take photo
+            //noinspection deprecation
+            startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+        }
+    }
+
+    @SuppressWarnings("deprecation")
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                //by this point we have the camera photo on disk
+                Bitmap takenImage = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
+                //RESIZE BITMAP, see section below
+                //load the taken image into a preview
+                ivItem.setImageBitmap(takenImage);
+            } else { // Result was a failure
+                Toast.makeText(this, "Error taking picture!", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private File getPhotoFileUri(String photoFileName) {
+        //get safe storage directory for photos
+        //use `getExternalFilesDir` on Context to access package-specific directories.
+        //this way, we don't need to request external read/write runtime permissions.
+        File mediaStorageDir = new File(this.getExternalFilesDir(Environment.DIRECTORY_PICTURES), TAG);
+
+        //create the storage directory if it does not exist
+        if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()) { Log.d(TAG, "Failed to create directory"); }
+        //return the file target for the photo based on filename
+        return new File(mediaStorageDir.getPath() + File.separator + photoFileName);
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
