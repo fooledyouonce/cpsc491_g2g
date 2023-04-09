1a943ec17baa4fe82e7b3e543df4b8deb567527a
Emily C <64298603+fooledyouonce@users.noreply.github.com>
2023-04-09 13:02:39 -0700

    Update WikiFragment.java

--- app/src/main/java/com/example/fh_2023_tecs/fragments/WikiFragment.java
+++ app/src/main/java/com/example/fh_2023_tecs/fragments/WikiFragment.java
@@ -1,10 +1,14 @@
-package com.example.fh_2023_tecs.fragments;
+package fragments;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.fh_2023_tecs.R;
@@ -14,15 +18,24 @@ public class WikiFragment extends Fragment {
     public WikiFragment() {
         // Required empty public constructor
     }
-
+    ImageButton btnPaper;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_wiki, container, false);
-
-        // set up any views or event listeners for the fragment
-
         return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        btnPaper = view.findViewById(R.id.btnPaper);
+
+        btnPaper.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "You Clicked the button!", Toast.LENGTH_LONG).show();
     }
 }
