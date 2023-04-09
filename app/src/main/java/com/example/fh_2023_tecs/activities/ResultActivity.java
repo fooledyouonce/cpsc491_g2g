4382ceb9d3aeb1bdb3cc9d6abf0f91a312efef78
Emily Crowl <emilycrowl2k@gmail.com>
2023-04-09 11:51:15 -0700

    fixed bug

--- app/src/main/java/com/example/fh_2023_tecs/activities/ResultActivity.java
+++ app/src/main/java/com/example/fh_2023_tecs/activities/ResultActivity.java
@@ -2,7 +2,6 @@ package com.example.fh_2023_tecs.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
@@ -23,21 +22,16 @@ public class ResultActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
 
-        tvItem.findViewById(R.id.tvItem);
-        tvRecycle.findViewById(R.id.tvRecycle);
-        tvWhere.findViewById(R.id.tvWhere);
-        btnReturn.findViewById(R.id.btnReturn);
+        tvItem = findViewById(R.id.tvItem);
+        tvRecycle = findViewById(R.id.tvRecycle);
+        tvWhere = findViewById(R.id.tvWhere);
+        btnReturn = findViewById(R.id.btnReturn);
 
         tvItem.setText("Test");
         tvRecycle.setText("Test");
         tvWhere.setText("Test");
 
-        btnReturn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                goMainActivity();
-            }
-        });
+        btnReturn.setOnClickListener(view -> goMainActivity());
     }
 
     private void goMainActivity() {
