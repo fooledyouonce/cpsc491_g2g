8141fd1415884a0ce459de43007725bf933fe74a
Emily Crowl <emilycrowl2k@gmail.com>
2023-04-08 21:39:19 -0700

    base files for new activities

---
+++ app/src/main/java/com/example/fh_2023_tecs/activities/ManualActivity.java
@@ -0,0 +1,4 @@
+package com.example.fh_2023_tecs.activities;
+
+public class ManualActivity {
+}
--- app/src/main/java/fragments/HomeFragment.java
+++ app/src/main/java/fragments/HomeFragment.java
@@ -1,24 +1,65 @@
 package fragments;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
 import com.example.fh_2023_tecs.R;
 
-import androidx.fragment.app.Fragment;
+import com.example.fh_2023_tecs.activities.ScanActivity;
+
+import com.example.fh_2023_tecs.activities.ManualActivity;
 
 public class HomeFragment extends Fragment {
 
-    public HomeFragment() {
-        // Required empty public constructor
-    }
+    Button btnScan;
+    Button btnManual;
+
+    public HomeFragment() {/*Required empty public constructor*/}
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_home, container, false);
-        // set up any views or event listeners for the fragment
-        return view;
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        btnScan = view.findViewById(R.id.btnScan);
+        btnManual = view.findViewById(R.id.btnManual);
+
+        btnScan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goScanActivity();
+            }
+        });
+
+        btnManual.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goManualActivity();
+            }
+        });
+    }
+
+    private void goScanActivity() {
+        Intent i = new Intent(getActivity(), ScanActivity.class);
+        startActivity(i);
+    }
+
+    private void goManualActivity() {
+        Intent i = new Intent(getActivity(), ManualActivity.class);
+        startActivity(i);
     }
 }
