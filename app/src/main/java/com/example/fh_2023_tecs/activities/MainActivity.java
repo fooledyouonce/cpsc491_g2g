680f9c90afd2d82782c78a6cd3933c320a6c4d73
Emily Crowl <emilycrowl2k@gmail.com>
2023-04-08 19:23:47 -0700

    implemented bottom nav view and logout.xml

--- app/src/main/java/com/example/fh_2023_tecs/activities/MainActivity.java
+++ app/src/main/java/com/example/fh_2023_tecs/activities/MainActivity.java
@@ -2,33 +2,71 @@ package com.example.fh_2023_tecs.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.fh_2023_tecs.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.parse.ParseUser;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btnLogout;
+    private BottomNavigationView bottomNavigationView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnLogout = findViewById(R.id.btnLogout);
-        btnLogout.setOnClickListener(new View.OnClickListener() {
+        ActionBar actionBar;
+        actionBar = getSupportActionBar();
+
+        bottomNavigationView = findViewById(R.id.bottomNavigation);
+
+        //needs work
+        /*bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @SuppressLint("NonConstantResourceId")
             @Override
-            public void onClick(View view) {
-                ParseUser.logOut();;
-                ParseUser currentUser = ParseUser.getCurrentUser();
-                goLoginActivity();
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment = null;
+                switch (item.getItemId()) {
+                    case R.id.action_home:
+                        fragment = new PostsFragment();
+                        break;
+                    case R.id.action_wiki:
+                        fragment = new ComposeFragment();
+                        break;
+                    case R.id.action_profile:
+                        fragment = new ProfileFragment(ParseUser.getCurrentUser());
+                    default:
+                        break;
+                }
+                fragmentManager.beginTransaction().replace(R.id.flContainer, Objects.requireNonNull(fragment)).commit();
+                return true;
             }
         });
+        bottomNavigationView.setSelectedItemId(R.id.action_home);
+    }*/
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.logout, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.Logout) {
+            ParseUser.logOut();
+            ParseUser currentUser = ParseUser.getCurrentUser();
+            goLoginActivity();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
     private void goLoginActivity() {
         Intent i = new Intent(this, LoginActivity.class);
         startActivity(i);
